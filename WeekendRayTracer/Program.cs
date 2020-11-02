f7608f804ac98dde07690090fae20dd12ecee41f
Erik Alenvall <erik@wehype.it>
2020-11-02 21:13:43 +0100

    Created solution

---
+++ WeekendRayTracer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeekendRayTracer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
