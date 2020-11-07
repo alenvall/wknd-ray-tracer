558b0da8a063c1df728f49495f9a01c89a46c440
alenvall <erik.alenvall@gmail.com>
2020-11-07 13:36:56 +0100

    Now able to render final scene of article, abstracted Camera setup and World generation into own methods

--- WeekendRayTracer/RayTracer.cs
+++ WeekendRayTracer/RayTracer.cs
@@ -20,27 +20,8 @@ namespace WeekendRayTracer
             int samplesPerPixel = 100;
             int maxDepth = 50;
 
-            // World
-            HittableList world = new HittableList();
-
-            var materialGround = new Lambertian(new Vec3(0.8, 0.8, 0.0));
-            var materialCenter = new Lambertian(new Vec3(0.1, 0.2, 0.5));
-            var materialLeft = new Dielectric(1.5);
-            var materialRight = new Metal(new Vec3(0.8, 0.6, 0.2), 0.0);
-
-            world.Add(new Sphere(new Vec3(0.0, -100.5, -1.0), 100.0, materialGround));
-            world.Add(new Sphere(new Vec3(0.0, 0.0, -1.0), 0.5, materialCenter));
-            world.Add(new Sphere(new Vec3(-1.0, 0.0, -1.0), 0.5, materialLeft));
-            world.Add(new Sphere(new Vec3(-1.0, 0.0, -1.0), -0.4, materialLeft));
-            world.Add(new Sphere(new Vec3(1.0, 0.0, -1.0), 0.5, materialRight));
-
-            var lookFrom = new Vec3(3, 3, 2);
-            var lookAt = new Vec3(0, 0, -1);
-            var vUp = new Vec3(0, 1, 0);
-            var focusDistance = (lookFrom - lookAt).Length();
-            var aperture = 2.0;
-
-            Camera cam = new Camera(lookFrom, lookAt, vUp, 20, aspectRatio, aperture, focusDistance);
+            var cam = SetupStandardCamera(aspectRatio);
+            var world = GenerateRandomScene();
 
             Log("Creating image...");
 
@@ -97,6 +78,101 @@ namespace WeekendRayTracer
             var directionUnit = ray.Direction.Unit();
             var t = 0.5 * (directionUnit.Y + 1.0);
             return (1.0 - t) * new Vec3(1.0, 1.0, 1.0) + t * new Vec3(0.5, 0.7, 1.0);
+        }
+
+        private HittableList GenerateRandomScene()
+        {
+            var world = new HittableList();
+
+            var groundMaterial = new Lambertian(new Vec3(0.5, 0.5, 0.5));
+            world.Add(new Sphere(new Vec3(0, -1000, 0), 1000, groundMaterial));
+
+            for (int a = -11; a < 11; a++)
+            {
+                for (int b = -11; b < 11; b++)
+                {
+                    var chooseMaterial = rand.NextDouble();
+                    var center = new Vec3(a + 0.9 + rand.NextDouble(), 0.2, b + 0.9 * rand.NextDouble());
+
+                    if ((center - new Vec3(4, 0.2, 0)).Length() > 0.9)
+                    {
+                        IMaterial sphereMaterial;
+
+                        if (chooseMaterial < 0.8)
+                        {
+                            // Diffuse
+                            var albedo = Vec3.Random() * Vec3.Random();
+                            sphereMaterial = new Lambertian(albedo);
+                            world.Add(new Sphere(center, 0.2, sphereMaterial));
+                        }
+                        else if (chooseMaterial < 0.95)
+                        {
+                            // Metal
+                            var albedo = Vec3.Random(0.5, 1);
+                            var fuzz = rand.NextDouble(0, 0.5);
+                            sphereMaterial = new Metal(albedo, fuzz);
+                            world.Add(new Sphere(center, 0.2, sphereMaterial));
+                        }
+                        else
+                        {
+                            // Glass
+                            sphereMaterial = new Dielectric(1.5);
+                            world.Add(new Sphere(center, 0.2, sphereMaterial));
+                        }
+                    }
+                }
+            }
+
+            var material1 = new Dielectric(1.5);
+            world.Add(new Sphere(new Vec3(0, 1, 0), 1.0, material1));
+
+            var material2 = new Lambertian(new Vec3(0.4, 0.2, 0.1));
+            world.Add(new Sphere(new Vec3(-4, 1, 0), 1.0, material2));
+
+            var material3 = new Metal(new Vec3(0.7, 0.6, 0.5), 0.0);
+            world.Add(new Sphere(new Vec3(4, 1, 0), 1.0, material3));
+
+            return world;
+        }
+
+        private Camera SetupStandardCamera(double aspectRatio)
+        {
+            var lookFrom = new Vec3(13, 2, 3);
+            var lookAt = new Vec3(0, 0, 0);
+            var vUp = new Vec3(0, 1, 0);
+            var focusDistance = 10.0;
+            var aperture = 0.1;
+
+            return new Camera(lookFrom, lookAt, vUp, 20, aspectRatio, aperture, focusDistance);
+        }
+
+        private HittableList GenerateSimpleScene()
+        {
+            HittableList world = new HittableList();
+
+            var materialGround = new Lambertian(new Vec3(0.8, 0.8, 0.0));
+            var materialCenter = new Lambertian(new Vec3(0.1, 0.2, 0.5));
+            var materialLeft = new Dielectric(1.5);
+            var materialRight = new Metal(new Vec3(0.8, 0.6, 0.2), 0.0);
+
+            world.Add(new Sphere(new Vec3(0.0, -100.5, -1.0), 100.0, materialGround));
+            world.Add(new Sphere(new Vec3(0.0, 0.0, -1.0), 0.5, materialCenter));
+            world.Add(new Sphere(new Vec3(-1.0, 0.0, -1.0), 0.5, materialLeft));
+            world.Add(new Sphere(new Vec3(-1.0, 0.0, -1.0), -0.4, materialLeft));
+            world.Add(new Sphere(new Vec3(1.0, 0.0, -1.0), 0.5, materialRight));
+
+            return world;
+        }
+
+        private Camera SetupSimpleCamera(double aspectRatio)
+        {
+            var lookFrom = new Vec3(3, 3, 2);
+            var lookAt = new Vec3(0, 0, -1);
+            var vUp = new Vec3(0, 1, 0);
+            var focusDistance = (lookFrom - lookAt).Length();
+            var aperture = 0.1;
+
+            return new Camera(lookFrom, lookAt, vUp, 20, aspectRatio, aperture, focusDistance);
         }
 
         private void Log(object text)
